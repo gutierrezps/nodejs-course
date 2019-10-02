c532a5cbcc7c20a3776a74d231b43290376b9979
Gutierrez PS <gutierrez@live.jp>
2019-10-02 00:20:36 -0300

    Added handlebars and converted html's to hbs'

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,6 +8,32 @@ const app = express()
 const publicDirPath = path.join(__dirname, '..', 'public')
 app.use(express.static(publicDirPath))
 
+app.set('view engine', 'hbs')
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        author: 'Gutierrez PS'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather App',
+        author: 'Gutierrez PS'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Weather App',
+        faqs: [
+            {question: 'How to do this?', answer: 'By doing that'},
+            {question: 'And how to do that?', answer: 'By doing something else'}
+        ]
+    })
+})
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast: {
