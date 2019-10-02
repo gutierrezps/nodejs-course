d34c92fbbdfb2d2c1cd9f5858944acecd76f0ad7
Gutierrez PS <gutierrez@live.jp>
2019-10-02 01:17:37 -0300

    Moved views to templates/views and added partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,43 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 // Create an Express application
 const app = express()
 
-// Add public path folder to be served by Express
 const publicDirPath = path.join(__dirname, '..', 'public')
+const viewsPath = path.join(__dirname, '..', 'templates', 'views')
+const partialsPath = path.join(__dirname, '..', 'templates', 'partials')
+
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set public path folder to be served by Express
 app.use(express.static(publicDirPath))
 
 app.set('view engine', 'hbs')
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
+        appTitle: 'Weather App',
+        pageTitle: 'Homepage',
         author: 'Gutierrez PS'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'Weather App',
+        appTitle: 'Weather App',
+        pageTitle: 'About',
         author: 'Gutierrez PS'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Weather App',
+        appTitle: 'Weather App',
+        pageTitle: 'Help',
+        author: 'Gutierrez PS',
         faqs: [
             {question: 'How to do this?', answer: 'By doing that'},
             {question: 'And how to do that?', answer: 'By doing something else'}
@@ -41,6 +52,24 @@ app.get('/weather', (req, res) => {
             precipitation: 0.2
         },
         location: 'João Pessoa, Paraíba'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        appTitle: 'Weather App',
+        pageTitle: 'Not found',
+        author: 'Gutierrez PS',
+        missing: 'Help article'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        appTitle: 'Weather App',
+        pageTitle: 'Not found',
+        author: 'Gutierrez PS',
+        missing: 'Page'
     })
 })
 
