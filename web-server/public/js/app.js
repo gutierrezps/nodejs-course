4130be946d3416f9138ef71e9ab7567c1eaf569a
Gutierrez PS <gutierrez@live.jp>
2019-10-03 16:12:51 -0300

    Added forecast fetch on frontend

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,15 @@
 console.log('Client side javascript file loaded')
+
+fetch('/weather?address=Joao Pessoa').then((response) => {
+    response.json().then((data) => {
+        const {error, location, forecast} = data
+        if (error) {
+            console.error(error)
+        } else if (location && forecast) {
+            console.log('Location:', location)
+            console.log('Forecast:', forecast)
+        } else {
+            console.error('Unknown response:', data)
+        }
+    })
+})
