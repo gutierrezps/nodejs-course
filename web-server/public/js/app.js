2f921170feca5b8b3abd5df2e7cbc8f9c3b86495
Gutierrez PS <gutierrez@live.jp>
2019-10-04 01:02:53 -0300

    Added location search form and now fetch at submit

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,15 +1,23 @@
 console.log('Client side javascript file loaded')
 
-fetch('/weather?address=Joao Pessoa').then((response) => {
-    response.json().then((data) => {
-        const {error, location, forecast} = data
-        if (error) {
-            console.error(error)
-        } else if (location && forecast) {
-            console.log('Location:', location)
-            console.log('Forecast:', forecast)
-        } else {
-            console.error('Unknown response:', data)
-        }
+const weatherForm = document.querySelector('form')
+const searchInput = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+    const location = searchInput.value
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            const {error, location, forecast} = data
+            if (error) {
+                console.error(error)
+            } else if (location && forecast) {
+                console.log('Location:', location)
+                console.log('Forecast:', forecast)
+            } else {
+                console.error('Unknown response:', data)
+            }
+        })
     })
 })
